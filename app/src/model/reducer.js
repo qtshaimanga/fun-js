cf24a08c673eb67f49aa4eb9191fc63f28d8b10b
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-08 12:26:26 +0200

    use action constants

--- app/src/model/reducer.js
+++ app/src/model/reducer.js
@@ -14,6 +14,7 @@ import {
   view
 } from 'ramda';
 
+import { ADD_TODO, TOGGLE_ALL, TOGGLE_ONE, DESTROY, CLEAR_COMPLETED } from './actions';
 
 const completedLens = lensProp('completed');
 const isActive = compose(not, view(completedLens));
@@ -23,15 +24,15 @@ const setCompleted = set(completedLens);
 
 const transform = (action) => {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return append(action.todo);
-    case 'TOGGLE_ALL':
+    case TOGGLE_ALL:
       return map(setCompleted(action.completed));
-    case 'TOGGLE_ONE':
+    case TOGGLE_ONE:
       return map(ifElse(equals(action.todo), toggleCompleted, identity));
-    case 'DESTROY':
+    case DESTROY:
       return without([action.todo]);
-    case 'CLEAR_COMPLETED':
+    case CLEAR_COMPLETED:
       return filter(isActive)
     default:
       return identity;
