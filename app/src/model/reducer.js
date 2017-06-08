6c14d4aed3dc0baa7a79114b767b754d1bd7518c
Wolfgang Goedel <wgoedel@gmail.com>
2017-06-08 12:22:07 +0200

    do

--- app/src/model/reducer.js
+++ app/src/model/reducer.js
@@ -22,30 +22,6 @@ import { ADD_TODO, TOGGLE_ALL, TOGGLE_ONE, DESTROY, CLEAR_COMPLETED } from './ac
 // I have used all of the functions above ;)
 // use the action constants in your switch statement
 
-const completedLens = lensProp('completed');
-const isActive = compose(not, view(completedLens));
-const toggleCompleted = over(completedLens, not);
-const setCompleted = set(completedLens);
-
-
-const transform = (action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return append(action.todo);
-    case TOGGLE_ALL:
-      return map(setCompleted(action.completed));
-    case TOGGLE_ONE:
-      return map(ifElse(equals(action.todo), toggleCompleted, identity));
-    case DESTROY:
-      return without([action.todo]);
-    case CLEAR_COMPLETED:
-      return filter(isActive)
-    default:
-      return identity;
-  }
-};
-
-
-export default (todos = [], action) => {
-  return transform(action)(todos); 
+export default (todos, action) => {
+  return todos; 
 };
